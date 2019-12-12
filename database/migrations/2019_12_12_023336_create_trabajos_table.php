66ebc6e1ac38636d37ce5892de3eed71b5c83299
Jordan <jordan_j9@hotmail.com>
2019-12-11 21:42:49 -0500

    Version 17.4 11 de Diciembre de 2019 21:42 JORDAN

---
+++ database/migrations/2019_12_12_023336_create_trabajos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrabajosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trabajos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->string('acta');
+            $table->date('fecha');
+            $table->date('iniciacion');
+            $table->date('terminacion');
+            $table->integer('hora_semana');
+            $table->string('descripcion');
+            $table->string('institucion');
+            $table->bigInteger('item_id')->unsigned();
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trabajos');
+    }
+}
